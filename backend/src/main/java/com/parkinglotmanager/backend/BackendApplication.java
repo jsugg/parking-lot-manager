a553f1419ec2f9f999df9b93c9d57bc2ac32e576
Juan Sugg <juanpedrosugg@gmail.com>
2023-10-09 14:22:01 -0300

    Backend DB Config

--- backend/src/main/java/com/parkinglotmanager/backend/BackendApplication.java
+++ backend/src/main/java/com/parkinglotmanager/backend/BackendApplication.java
@@ -3,11 +3,18 @@ package com.parkinglotmanager.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
